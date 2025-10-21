c9374f68eb51601bc0b69b2764ff08fc3b260e2f
quan.u <quan.u@seltec.io>
2025-10-21 10:55:10 +0700

    hard reset to convert to mini

--- ftplugin/java.lua
+++
@@ -1,7 +0,0 @@
--- See `:help vim.lsp.start` for an overview of the supported `config` options.
-local config = {
-  cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
-
-  root_dir = vim.fs.root(0, { "gradlew", ".git", "mvnw" }),
-}
-require("jdtls").start_or_attach(config)
