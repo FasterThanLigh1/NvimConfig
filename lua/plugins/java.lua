5c14c6b23f59634c26fabc7a59c2bbe276ab8a0c
quan.u <quan.u@seltec.io>
2026-01-16 19:22:27 +0700

    Add front end

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -3,6 +3,21 @@ return {
   lazy = false,
   config = function()
     require('java').setup()
+    vim.lsp.config('jdtls', {
+  settings = {
+    java = {
+      configuration = {
+        runtimes = {
+          {
+            name = "SdkmanJava",
+            path = "$JAVA_HOME",
+            default = true,
+          }
+        }
+      }
+    }
+  }
+})
     vim.lsp.enable('jdtls')
   end,
 }
